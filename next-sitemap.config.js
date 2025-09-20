14bead4b45d8bfe935acf14b780b71ae5eb5e646
lckhndleup <maliyildiz0307@gmail.com>
2025-09-20 17:58:54 +0300

    fixed

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: "https://jenesissteam.com", // kendi domainin
+  siteUrl: "https://jenesis.com.tr", // kendi domainin
   generateRobotsTxt: true, // otomatik robots.txt üret
   sitemapSize: 7000, // çok fazla sayfa olursa parçalar
   changefreq: "weekly",
